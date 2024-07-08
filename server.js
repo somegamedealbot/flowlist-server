72e4233bb1ecf79a49b5101a601cee14954c6c59
somegamedealbot <bzhang575@gmail.com>
2024-07-08 19:48:20 -0400

    fixed listening port

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/logout', errorHandleWrapper(async(req, res) => {
     return {};
 }));
 
-app.listen(port, () => {
+app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
 })
 
