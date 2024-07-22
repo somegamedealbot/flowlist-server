14a7553d987348e8323ea19ef4ec203970a8f50b
somegamedealbot <bzhang575@gmail.com>
2024-07-22 00:38:09 -0400

    changes

--- db/dynamo-client.js
+++ db/dynamo-client.js
@@ -4,15 +4,9 @@ const {DynamoDBClient} = require('@aws-sdk/client-dynamodb')
 
 const createClient = () => {
     return new DynamoDBClient({
-        // use for local database development
-        // credentials: {
-        //     accessKeyId: 'FakeKey',
-        //     secretAccessKey: 'FakeAccessKey',
-        // },
-        // endpoint: 'http://localhost:8000'
-        
         region: process.env.DB_REGION,  
-        credentials: fromSSO({
+
+        credentials: fromSSO({ // use for local database development
             profile: "bill-dev"
         })
     }) 
